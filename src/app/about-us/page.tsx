3969259c323544a6b9a43956471dd4430dad7841
Niño Mollaneda <ninom@Mr-Meeseeks.local>
2024-11-21 21:13:36 +0800

    add about us page

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutUsPage() {
+  return (
+    <div className="flex place-content-center flex-col gap-2">
+      <img src="https://drive.google.com/thumbnail?id=1jvhJunxWdc6QM3EUzLR-vyydI4ibgPKb&sz=w900" />
+      <img src="https://drive.google.com/thumbnail?id=1lLCuwI2P-Dy1UVQaSTP4DeSBctXEArcT&sz=w900" />
+    </div>
+  );
+}
